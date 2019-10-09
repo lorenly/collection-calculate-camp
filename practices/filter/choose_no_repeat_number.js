e84a65ec276992ef5db9acb34877e4ac606e9ecd
LORENLY <lyrol.lorenzo@oocl.com>
2019-10-10 04:44:05 +0800

    created filter function

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -3,6 +3,7 @@
 function choose_no_repeat_number(collection) {
 
   //write code here
+  return collection.filter((index, num, self) => self.indexOf(index) === num);
 }
 
 module.exports = choose_no_repeat_number;
