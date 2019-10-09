df46d93f7b1a7a53fbd6e815819bc62e9b2866f4
LORENLY <lyrol.lorenzo@oocl.com>
2019-10-10 04:44:35 +0800

    Created group element

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,13 @@
 function grouping_count(collection) {
 
   //write code here
+  let uniqueElement = collection.filter((index, num, self) => self.indexOf(index) === num);
+  let groupElement = {}
+  
+  for(let key of uniqueElement)
+  	groupElement[key] = collection.filter(num => num === key).length;
+  
+  return groupElement;
 }
 
 module.exports = grouping_count;
