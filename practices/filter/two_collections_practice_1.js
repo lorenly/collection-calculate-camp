851d278499a327ed7f46fb5eee9b5bc7ab4b8523
LORENLY <lyrol.lorenzo@oocl.com>
2019-10-10 05:16:15 +0800

    Created filter function

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -3,6 +3,7 @@
 function choose_common_elements(collection_a, collection_b) {
 
   //write code here
+  return collection_a.filter(element => collection_b.indexOf(element) >= 0);
 }
 
 module.exports = choose_common_elements;
--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -3,6 +3,12 @@
 function choose_divisible_integer(collection_a, collection_b) {
 
   //write code here
+  let divisibleInt = []
+  collection_a.forEach(elementA => {
+  	if(collection_b.filter(elementB => elementA % elementB === 0).length > 0)
+  		divisibleInt.push(elementA);
+  });
+  return divisibleInt;
 }
 
 module.exports = choose_divisible_integer;
