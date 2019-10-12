035e3e52c6a7751583807189d577f4d033a4139e
LORENLY <lyrol.lorenzo@oocl.com>
2019-10-13 02:16:30 +0800

    Test passed

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -2,6 +2,25 @@
 
 function get_letter_interval_2(number_a, number_b) {
   //write code here
+  let min = Math.min(number_a, number_b);
+  let max = Math.max(number_a, number_b);
+  const maxAscii = 122 //z
+  let letterInterval = [];
+
+  for(min; min <= max; min++){
+  	letterInterval.push(min);
+  }
+
+  letterInterval = letterInterval.map((ascii, index) => { 
+  	ascii += 70;
+  	return index < 7 ? ascii+26 > maxAscii ? 'a' + String.fromCharCode(ascii) : 
+  		String.fromCharCode(ascii + 26) : 
+  		ascii > maxAscii ? 'b' + String.fromCharCode(ascii - 26) :
+  		'a' + String.fromCharCode(ascii) 
+  		 
+  });
+
+  return number_a <= number_b ? letterInterval : letterInterval.reverse();
 }
 
 module.exports = get_letter_interval_2;
