6b89dc3f151a938c5851757e8241a567c0db326a
LORENLY <lyrol.lorenzo@oocl.com>
2019-10-13 00:58:34 +0800

    Test Passed

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -2,6 +2,17 @@
 
 function get_letter_interval(number_a, number_b) {
   //write code here
+  let min = Math.min(number_a, number_b);
+  let max = Math.max(number_a, number_b);
+  let letterInterval = []
+  
+  for(min; min <= max; min++){
+  	letterInterval.push(min);
+  }
+
+  letterInterval = letterInterval.map(ascii => String.fromCharCode(ascii+96));
+  
+  return (number_a <= number_b) ? letterInterval : letterInterval.reverse();
 }
 
 module.exports = get_letter_interval;
