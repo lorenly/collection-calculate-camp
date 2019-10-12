a2465c80d7e92ce9615766bfe5154314f65b5391
LORENLY <lyrol.lorenzo@oocl.com>
2019-10-12 19:49:12 +0800

    Passed the test

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -2,6 +2,15 @@
 
 function get_integer_interval(number_a, number_b) {
   //write code here
+  let min = Math.min(number_a, number_b);
+  let max = Math.max(number_a, number_b);
+  let intervalResult = [];
+  
+  for(min; min <= max; min++){
+  	intervalResult.push(min);
+  }
+
+  return number_a < number_b ? intervalResult : intervalResult.reverse();
 }
 
 module.exports = get_integer_interval;
