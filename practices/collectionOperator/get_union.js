e96ff599e61798fe58e00037c3090ddb2cabc16a
LORENLY <lyrol.lorenzo@oocl.com>
2019-10-13 02:29:21 +0800

    test passed

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -2,6 +2,10 @@
 
 function get_union(collection_a, collection_b) {
   //write code here
+  let unionResult = [];
+  unionResult = collection_a.concat(collection_b);
+  
+  return unionResult.filter((element, index) => unionResult.indexOf(element) == index);
 }
 
 module.exports = get_union;
