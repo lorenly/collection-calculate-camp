f11f6248fe5d5135164bbb829509f56938243002
LORENLY <lyrol.lorenzo@oocl.com>
2019-10-13 18:11:45 +0800

    Test passed

--- practices/map/rank_asc.js
+++ practices/map/rank_asc.js
@@ -1,6 +1,6 @@
 'use strict';
 var rank_asc = function(collection){
-  return [6,5,4,3,2];
+  return collection.sort((num, cur) => cur - num);
 };
 
 module.exports = rank_asc;
--- practices/map/rank_desc.js
+++ practices/map/rank_desc.js
@@ -1,6 +1,6 @@
 'use strict';
 var rank_desc = function(collection){
-  return [2,3,4,5,6];
+  return collection.sort((num, cur) => num - cur);
 };
 
 module.exports = rank_desc;
