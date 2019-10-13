03bc892338ca9ef43dfb73358c13a450a9696aee
LORENLY <lyrol.lorenzo@oocl.com>
2019-10-13 18:29:12 +0800

    Test passed

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -2,6 +2,7 @@
 
 function compare_collections(collection_a, collection_b) {
   //在这里写入代码
+  return collection_a.reduce((accu, cur) => collection_b.includes(cur) ? true : false);
 }
 
 module.exports = compare_collections;
