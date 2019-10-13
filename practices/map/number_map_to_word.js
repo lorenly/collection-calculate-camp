1d9c851c90b6ed9cda0ff7438fee148e1002aed8
LORENLY <lyrol.lorenzo@oocl.com>
2019-10-13 17:50:41 +0800

    Test passed

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,6 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  return collection.map(num => String.fromCharCode(num + 96));
 };
 
 module.exports = number_map_to_word;
