b5951e15d38b19b0cf468deb20fd2de1c7ed0d26
LORENLY <lyrol.lorenzo@oocl.com>
2019-10-13 20:30:11 +0800

    Test passed

--- practices/reduce/find_first_even.js
+++ practices/reduce/find_first_even.js
@@ -2,6 +2,13 @@
 
 function find_first_even(collection) {
   //write code here
+  let findFirstElement = false;
+  return collection.reduce((num, curr, index) => {
+  	return (curr % 2 == 0 && findFirstElement == false) ? (
+  													findFirstElement = true, curr 
+  												) : num;
+  });
+  return collection.reduce((num, curr, index) => curr % 2  == 0? curr : num)
 }
 
 module.exports = find_first_even;
--- practices/reduce/find_last_even.js
+++ practices/reduce/find_last_even.js
@@ -2,6 +2,7 @@
 
 function find_last_even(collection) {
   //write code here
+  return collection.reduce((num, curr, index) => curr % 2  == 0? curr : num)
 }
 
 module.exports = find_last_even;
