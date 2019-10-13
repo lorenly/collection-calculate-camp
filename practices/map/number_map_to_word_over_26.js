8210d9f4e8cdfe9a05be026aed59077b3d4d5ce5
LORENLY <lyrol.lorenzo@oocl.com>
2019-10-13 17:56:57 +0800

    Test passed

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,8 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  return collection.map(num =>{
+  	return num > 26 ? "a" + String.fromCharCode((num - 26) + 96) : String.fromCharCode(num + 96); 
+  });
 };
 
 module.exports = number_map_to_word_over_26;
