af6182432c7491d2093c0c356495c858b6a0f908
LORENLY <lyrol.lorenzo@oocl.com>
2019-10-13 18:55:36 +0800

    Test passed

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -2,6 +2,12 @@
 
 function compute_chain_median(collection) {
   //write code here
+  const collectionLength = collection.length;
+  return collection.split('->')
+  			.sort((num, cur) => num - cur)
+  			.reduce((num, index) =>{
+  				return collectionLength % 2 == 0 ? collection[collectionLength/2] : collection[collectionLength / 2 - 0.5 ] -1 + 0.5 ;
+  			});
 }
 
 module.exports = compute_chain_median;
